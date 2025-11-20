cf4728e16335cda5fb36d1c50775487bc403d06e
Jennifer Perez-Diaz <perezjennd@gmail.com>
2025-11-19 22:47:18 -0800

    Parse command-line arguments

---
+++ log_analyzer.py
@@ -0,0 +1,18 @@
+
+
+import argparse
+
+def main():
+    # Create argument parser
+    parser = argparse.ArgumentParser(description="Analyze log files")
+
+    parser.add_argument('filename', help='The log file to analyze')
+    parser.add_argument('--errors', action='store_true', help='Show only errors')
+    parser.add_argument('--export', help='Export results to JSON file')
+
+    # Parse the command-line arguments and store them
+    args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
